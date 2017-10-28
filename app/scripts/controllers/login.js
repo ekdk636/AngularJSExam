e39a4b6bf61f68118fc6fc5056e5327b8e04e680
ekdk636 <ekdk636@gmail.com>
2017-10-28 16:51:24 +0900

    20171028

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,11 +7,37 @@
  * # LoginCtrl
  * Controller of the angularJsexamApp
  */
-angular.module('angularJsexamApp')
-  .controller('LoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('angularJsexamApp').controller('LoginCtrl', 
+  	['$scope','$state','sessionInfo','sessionService', 
+  	function($scope, $state, sessionInfo, sessionService)
+  	{
+	    this.awesomeThings =
+	    [
+			'HTML5 Boilerplate',
+			'AngularJS',
+			'Karma'
+	    ];
+		
+		if (sessionInfo.isUserSignedIn()) {
+	    	$state.go('main');
+	    } else {
+	    	$state.go('login');
+	    }
+    $scope.submitLogin = function() {
+    	sessionService.login($scope.login, function(res) {
+    		$state.go('main');
+    	});
+    }
+    $scope.isUserSignedIn = function() {
+    	return sessionInfo.isUserSignedIn();
+    }
+    $scope.getUserId = function() {
+    	if (sessionInfo.isUserSignedIn())
+    		return sessionInfo.getCurrentUser().data.user_id;
+    	else return '';
+    }
+    $scope.logout = function() {
+    	sessionInfo.reset();
+    	$state.go('login');
+    }
+  }]);
